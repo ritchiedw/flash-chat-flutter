e0c4d1dffd727379dcf121089bea7b544411265b
Dave Ritchie <ritchiedw@hotmail.com>
2019-08-18 14:57:29 +0100

    listening for data using streams

--- lib/screens/chat_screen.dart
+++ lib/screens/chat_screen.dart
@@ -36,6 +36,21 @@ class _ChatScreenState extends State<ChatScreen> {
     }
   }
 
+//  void getMessage() async {
+//    final messages = await _firestore.collection('messages').getDocuments();
+//    for (var message in messages.documents) {
+//      print(message.data);
+//    }
+//  }
+
+  void messagesStream() async {
+    await for (var snapshot in _firestore.collection('messages').snapshots()) {
+      for (var message in snapshot.documents) {
+        print(message.data);
+      }
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
